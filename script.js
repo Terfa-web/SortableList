38e3acf8e880e57c5b94b5f45c6ae87a047f5f47
NOMHWANGE <afathelion@gmail.com>
2023-06-29 04:27:36 +0300

    #Add Some Extra Classes
    added zero opacity to class "item" that has active class "dragging"
    Added defer to Script tag in html doc
    Set draggable to true inhtml doc

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-const items = document.querySelectorAll('.item')
-const sortableList = document.querySelector('.sortable-list')
+
+const sortableList = document.querySelector('.sortable-list');
+const items = sortableList.querySelectorAll('.item');
 
 items.forEach(item => {
   //add dragging class to item on dragstart
   item.addEventListener('dragstart', () => {
     //adding dragging class to item after a delay
-    setTimeout(() => item.classList.add('dragging') ,0)
+    setTimeout(() => item.classList.add('dragging'),0)
     
   });
   //remove draggin class from item on dragend event
@@ -14,4 +15,22 @@ items.forEach(item => {
   })
 });
 
-sortableList.addEventListener('dragover', initSortableList());
+function initSortableList (e)  {
+
+  e.preventDefault();
+  const draggingItem = sortableList.querySelector('.dragging')
+  //Getting all items except currently dragging and making array of them
+  const siblings = [...sortableList.querySelectorAll('.item:not(.dragging)')];
+
+
+  //finding the sibling after which the draggin item should be placed
+  let nextSibling = siblings.find(sibling => {
+        return e.clientY <= sibling.offsetTop + sibling.offsetHeight / 2;
+  });
+  
+  //Inserting the draggin item before the found sibling
+  sortableList.insertBefore(draggingItem, nextSibling);
+}
+
+sortableList.addEventListener('dragover', initSortableList);
+sortableList.addEventListener('dragenter', e => e.preventDefault());
